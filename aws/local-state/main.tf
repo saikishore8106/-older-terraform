802e678c2a6c51a3f4f06ca02a34c4245da8e37f
saikishore8106 <145852787+saikishore8106@users.noreply.github.com>
2025-07-24 22:15:00 +0530

    Update main.tf

--- aws/local-state/main.tf
+++ aws/local-state/main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
+  required_version = ">= 1.12.2"
+}
 
+provider "aws" {
+    region ="eu-north-1"
+}
+
+resource "aws_instance" "app_server" {
+    ami = "ami-042b4708b1d05f512"
+    instance_type = "t3.micro"
+
+    tags = {
+       Name = "terraform_demo"
+    }
+}  
